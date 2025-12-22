22131b1a5a91477de358af0fdf5e5b4168b2fda7
oussamaelmessaoudi <oussama.elmessaoudi.39@edu.uiz.ac.ma>
2025-12-22 13:50:13 +0100

    feat(project-service): add DTOs for project requests and responses

--- backend/project-service/src/main/java/com/intern/projectservice/dto/ProjectRequest.java
+++ backend/project-service/src/main/java/com/intern/projectservice/dto/ProjectRequest.java
@@ -1,4 +1,13 @@
 package com.intern.projectservice.dto;
 
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class ProjectRequest {
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    private String description;
 }
